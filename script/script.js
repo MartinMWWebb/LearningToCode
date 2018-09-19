b97c633b309cd4faed087080f1c7948c43b4f37b
Martin Webb <MartinMWWebb@users.noreply.github.com>
2018-09-19 10:32:48 +0100

    V0.2
    Auto load data and filter based on user

--- script/script.js
+++ script/script.js
@@ -1,25 +1,22 @@
 
-function loadButton1() {
-  $.getJSON('https://jsonplaceholder.typicode.com/todos/1', function (data) {
-    var text = `UserId : ${data.userId}<br>
-                    id : ${data.id}<br>
-                    Title : ${data.title}<br>
-                    Completed : ${data.completed}`
-        $(".toDoData").html(text);
-          });
-      };
+function loadUserIds() {
+  let userDropdown = $("#userId-dropdown");
+  userDropdown.empty();
+  userDropdown.append('<option selected="true" disabled>Choose User</option>');
+  userDropdown.prop('selectedIndex', 0);
+  const url = 'https://jsonplaceholder.typicode.com/users';
+// Populate dropdown with list of userIds
+  $.getJSON(url, function (data) {
+    var uniqueUserIds = $.unique(data.map(function (i) {
+      return i.id;
+    }));
+    $.each(uniqueUserIds, function (key, val) {
+    userDropdown.append($('<option></option>').text(val).val(val));
+    })
+  });
+};
 
-function loadButton2() {
-  $.getJSON('https://jsonplaceholder.typicode.com/todos/2', function (data) {
-    var text = `UserId : ${data.userId}<br>
-                    id : ${data.id}<br>
-                    Title : ${data.title}<br>
-                    Completed : ${data.completed}`
-        $(".toDoData").html(text);
-          });
-      };
-
-function loadToTable() {
+function loadToDosToTable() {
   $.getJSON('https://jsonplaceholder.typicode.com/todos', function (data) {
     var toDos = [];
     $.each(data, function (key, val) {
@@ -32,21 +29,46 @@ function loadToTable() {
         + "</tr>"
       ));
     });
-    //$('.toDos').html(toDos);
     $('#toDoTable tr:last').after(toDos);
   });
 };
 
-
-/*
-//--No longer in use--//
-function loadButton3() {
-  $.getJSON('https://jsonplaceholder.typicode.com/posts', function (data) {
-    var toDos = [];
-    $.each(data, function (key, val) {
-      toDos.push("<p>" + (JSON.stringify(val)) + "</p>");
-    });
-    $('.toDos').html(toDos);
-  });
+function filter(){
+  var userData, completeData, userFilter, completeFilter
+  
+  userFilter = $("#userId-dropdown option:selected").text();
+  completeFilter = $("#completed-dropdown option:selected").text();
+  userData = $("#toDoTable td:nth-child(2)").text();
+  completeData = $("#toDoTable td:nth-child(4)").text();
+  console.log (
+    "UF", userFilter,
+    "CF", completeFilter,
+    "UD", userData,
+    "CD", completeData
+  );
+  
+  if (userData != userFilter && completeData != completeFilter){
+    $(this).parent().hide();
+  }
+  else {
+    $(this).parent().show();
+  }
 };
-*/
+
+$(document).ready(function() {
+  loadUserIds();
+  
+  loadToDosToTable();
+  
+    $("#userId-dropdown").on("change",function(){
+    var a = $(this).find("option:selected").html();
+    $("#toDoTable tr td:nth-child(2)").each(function(){
+      if($(this).html() != a){
+        $(this).parent().hide();
+      }
+      else{
+        $(this).parent().show();
+      };
+    });
+  });
+});
