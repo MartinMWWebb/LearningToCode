d65720d90634019c90a1c291359a2512d6fbdb39
Martin Webb <MartinMWWebb@users.noreply.github.com>
2018-09-24 14:58:05 +0100

    V0.5
    To-Dos from json placeholder does not accept PUT/Patch requests - final point with To-Dos - moving to 'posts'

--- script/script.js
+++ script/script.js
@@ -37,6 +37,23 @@ function loadToDosToTable() {
   });
 };
 
+function loadToDosToTableEdit() {
+  $.getJSON('https://jsonplaceholder.typicode.com/todos', function (data) {
+    var toDos = [];
+    $.each(data, function (key, val) {
+      toDos.push(
+        '<tr contenteditable="true">'
+        + '<td style="display:none">' + (JSON.stringify(val.id)) + "</td>"
+        + '<td>' + (JSON.stringify(val.userId) + "</td>" 
+        + '<td>' + (JSON.stringify(val.title)) + "</td>"
+        + '<td>' + (JSON.stringify(val.completed)) + "</td>"
+        + "</tr>"
+      ));
+    });
+    $('#toDoTable-editable tr:last').after(toDos);
+  });
+};
+
 //Work in progress to have multiple dropdowns working to filter data
 function filter(){
   var userData, completeData, userFilter, completeFilter
@@ -60,10 +77,11 @@ function filter(){
   }
 };
 
-//Creates a new to do on the server (faked by the API but logs to console)
+//Creates a new to do on the server (faked by the API but logs to console as a success)
 function createToDo(){
   var userDropdown = $("#userId-dropdown").val();
   var titleTextbox = $("#title-textbox").val();
+  var result = ("");
   const url = 'https://jsonplaceholder.typicode.com/todos';
   fetch(url, {
     method: 'POST',
@@ -80,10 +98,57 @@ function createToDo(){
   .then(json => console.log(json))
 };
 
+//Testing updating server 
+function updateToDos(){
+  var myObjects = [];
+  myObjects = getAllRows();
+  console.log(myObjects);
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  fetch(url, {
+    method: 'PATCH',
+    body: JSON.stringify({
+    id: myObjects.id,
+    userId: myObjects.userId,
+    title: myObjects.title,
+    completed: myObjects.completed
+  }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+};
+
+
+function getAllRows(){
+  var myObjects = [];
+  $('#toDoTable-editable tr').each(function (index, value){
+    var row = getRow(index);
+    myObjects.push(row);
+  });
+  //Removes the heading row
+  myObjects.shift();
+  return myObjects;
+};
+
+// Read the row into an object
+function getRow(rowNum){
+  var row = $('#toDoTable-editable tr').eq(rowNum);
+  var myObject = {};
+  myObject.id = row.find('td:eq(0)').text();
+  myObject.userId = row.find('td:eq(1)').text();
+  myObject.title = row.find('td:eq(2)').text();
+  myObject.completed = row.find('td:eq(3)').text();
+  return myObject;
+};
+
+
 $(document).ready(function() {
   //When page ready load userIds to dropdown and all todos into table.
   loadUserIds();
   loadToDosToTable();
+  loadToDosToTableEdit();
   
   //When userID is changed filter items in table
   $("#userId-dropdown").on("change",function(){
@@ -98,4 +163,17 @@ $(document).ready(function() {
     });
   });
   
+  //--for editable table test--- When userID is changed filter items in table
+  $("#userId-dropdown").on("change",function(){
+    var a = $(this).find("option:selected").html();
+    $("#toDoTable-editable tr td:nth-child(2)").each(function(){
+      if($(this).html() != a){
+        $(this).parent().hide();
+      }
+      else{
+        $(this).parent().show();
+      };
+    });
+  });
+
 });
